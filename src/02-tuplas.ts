93224ebfbb8c7b8f8fba2d483e10f88cb69d76b4
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-09 08:49:30 -0300

    feat: tuplas

---
+++ src/02-tuplas.ts
@@ -0,0 +1,7 @@
+const prices: (number | string )[] = [1,3,1,5,'asd'];
+
+const user: [string, number] = ['nicobytes',12]; // primer posicion un string y en la segunda un numero
+
+
+const [username, age] = user // desestructuro una tupla
+console.log(username, age)
