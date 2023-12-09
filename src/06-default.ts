d9797b0005a62ddaac63b3a59be6b6b60c7d297c
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-09 14:49:35 -0300

    feat: parámetros por defecto

---
+++ src/06-default.ts
@@ -0,0 +1,21 @@
+export const createProduct = (
+  id: string | number,
+  isNew: boolean = true,
+  stock:number = 10,
+) => {
+  return {
+    id,
+    //isNew: isNew || true, // el problema de el operador || es que si isNew es false pasará a true
+    isNew,
+    stock,
+  };
+};
+
+const p1 = createProduct(1,true,12)
+console.log(p1)
+
+const p2 = createProduct(1)
+console.log(p2)
+
+const p3 = createProduct(1, false, 0)
+console.log(p3)
