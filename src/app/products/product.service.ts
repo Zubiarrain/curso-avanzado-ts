a613f15b16381ec2a02b30a2ce88950b93b258eb
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-10 17:54:17 -0300

    feat: omit y pick

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,9 @@
 import { Product } from "./product.model";
+import { CreateProductDto } from "./product.dto";
+import { faker } from "@faker-js/faker";
 
 export const products: Product[] = [];
+
 const getIndexProduct = (id: string | number) => {
   const index = products.findIndex(product => product.id == id)
   index === -1 && throwError('Product not found')
@@ -9,8 +12,22 @@ const getIndexProduct = (id: string | number) => {
 }
 const throwError = (error:string) => new Error(error)
 
-export const addProduct = (data:Product) => {
-  products.push(data);
+export const addProduct = (data:CreateProductDto): Product => {
+  const newProduct = { //-> se haría en la BBDD
+    ...data,
+    id: faker.string.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    category: {
+      id: data.categoryId,
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    }
+
+  }
+  products.push(newProduct);
+  return newProduct;
 }
 
 export const getProduct = (id: string | number) => {
